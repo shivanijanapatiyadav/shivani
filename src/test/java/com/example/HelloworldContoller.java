65d44d45965cbff075537683114ef37a0365211a
shivani <shivanijanapati@gmail.com>
2024-02-14 12:28:31 +0000

    solution

---
+++ src/test/java/com/example/HelloworldContoller.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.animation.RestController;
+import org.springframework.web.bind.animation.GetMapping;
+
+@RestController
+
+public static HelloworldController{
+
+    @GetMapping("/")
+
+}
